05ac41ffac5679f81aadf04569cbaa9c3299234e
Dontbehigher <robert200202@163.com>
2024-01-31 12:37:04 +0800

    mint and display

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import inscribeData from './components/inscribeData.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import inscribeData from './components/inscribeData.vue'
+// import Vue from'vue'
+// import router from 'vue-router'
+// import mintData from'./components/mint.vue'
+// import display from'./components/display.vue'
+
 </script>
 
 <template>
-  <header>
-  </header>
-
-
-  <main>
-    <HelloWorld />
-    <inscribeData />
-  </main>
+  <div id="app" class="container">
+    <div id="nav">
+     <h1><router-link to="/display">display</router-link>  |</h1>
+      <h1><router-link to="/mint">mintData</router-link> </h1>
+    </div>
+    <router-view/>
+    </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-
-}
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    min-height: 100vh; /* 设置最小高度，确保内容撑开整个视口 */
+  }
+h1 {
+    color: #0a0909;
+    font-size: 32px;
+    margin-bottom: 13px;
+  }
 </style>
---
+++ src/components/display.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <h2>NFT查询</h2>
+
+    <!-- 获取所有 NFT -->
+    <div>
+      <h3>获取所有 NFT</h3>
+      <button @click="getNFTAll">获取所有 NFT</button>
+      <p>{{ nftAllData }}</p>
+    </div>
+
+    <!-- 获取可铸造的 FT -->
+    <div>
+      <h3>获取可铸造的 FT</h3>
+      <button @click="getFTCanMint">获取可铸造的 FT</button>
+      <p>{{ ftCanMintData }}</p>
+    </div>
+
+    <!-- 获取所有 FT -->
+    <div>
+      <h3>获取所有 FT</h3>
+      <button @click="getFTAll">获取所有 FT</button>
+      <p>{{ ftAllData }}</p>
+    </div>
+
+    <!-- 通过 ft_tick 获取 FT -->
+    <div>
+      <h3>通过 ft_tick 获取 FT</h3>
+      <input v-model="ftTick" placeholder="输入 ft_tick" />
+      <button @click="getFTByTick">获取 FT</button>
+      <p>{{ ftByTickData }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tokenPingData: "",
+      nftAllData: "",
+      ftCanMintData: "",
+      ftAllData: "",
+      ftTick: "",
+      ftByTickData: "",
+    };
+  },
+  methods: {
+    async getTokenPing() {
+      const response = await this.$axios.get("/token/ping");
+      this.tokenPingData = response.data;
+    },
+    async getNFTAll() {
+      const response = await this.$axios.get("/token/nft/all");
+      this.nftAllData = response.data;
+    },
+    async getFTCanMint() {
+      const response = await this.$axios.get("/token/ft/can_mint");
+      this.ftCanMintData = response.data;
+    },
+    async getFTAll() {
+      const response = await this.$axios.get("/token/ft/all");
+      this.ftAllData = response.data;
+    },
+    async getFTByTick() {
+      const response = await this.$axios.get(`/token/ft/${this.ftTick}`);
+      this.ftByTickData = response.data;
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add your component styles here */
+</style>
